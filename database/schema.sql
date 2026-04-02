89574267a478a9304b20e231a385d7d77ad9b89f
Ubuntu <alb.nazarian@gmail.com>
2026-04-02 10:13:55 +0000

    fixed: API non accessible

--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS dashboard_metrics;
+DROP TABLE IF EXISTS ml_features;
 DROP TABLE IF EXISTS candles;
 
 CREATE TABLE candles (
@@ -12,5 +14,30 @@ CREATE TABLE candles (
     close_time BIGINT NOT NULL,
     number_of_trades INTEGER,
     ingested_at BIGINT,
+    PRIMARY KEY (symbol, interval, open_time)
+);
+
+CREATE TABLE dashboard_metrics (
+    symbol TEXT NOT NULL,
+    interval TEXT NOT NULL,
+    last_price DOUBLE PRECISION,
+    max_price DOUBLE PRECISION,
+    min_price DOUBLE PRECISION,
+    avg_volume DOUBLE PRECISION,
+    ema_20 DOUBLE PRECISION,
+    rsi DOUBLE PRECISION,
+    PRIMARY KEY (symbol, interval)
+);
+
+CREATE TABLE ml_features (
+    symbol TEXT NOT NULL,
+    interval TEXT NOT NULL,
+    open_time BIGINT NOT NULL,
+    close DOUBLE PRECISION,
+    return_1h DOUBLE PRECISION,
+    ma_24 DOUBLE PRECISION,
+    volatility_24 DOUBLE PRECISION,
+    ema_20 DOUBLE PRECISION,
+    rsi DOUBLE PRECISION,
     PRIMARY KEY (symbol, interval, open_time)
 );
